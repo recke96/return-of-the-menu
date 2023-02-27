3ea8f6633e8a5e971ef0986c9700021e55b81059
Jakob Ecker <jakob.ecker@gmx.at>
2023-02-27 17:12:08 +0100

    initial commit

---
+++ src/lib/TheMenu.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import {Container, Icon, Row, Col} from 'svelte-chota';
+    import {mdiSync, mdiAlertCircle} from '@mdi/js';
+    import {fetchMenuData} from './menu';
+    import RestaurantCard from "./RestaurantCard.svelte";
+
+    export let date: Date;
+
+    $: data = fetchMenuData(date);
+</script>
+
+<Container style="margin-top: 2em">
+    {#await data}
+        <div class="is-center">
+            <Icon src={mdiSync} size="6" spin="1"/>
+        </div>
+    {:then restaurants}
+        <Row>
+            {#each restaurants as restaurant (restaurant.id)}
+                <Col size="12" sizeMD="12" sizeLG="6">
+                    <RestaurantCard {restaurant}/>
+                </Col>
+            {/each}
+        </Row>
+    {:catch err}
+        <div class="bg-error" style="padding: 10px; border-radius: 5px">
+            <span><Icon src={mdiAlertCircle} size="4"/></span>
+            {err.message}
+        </div>
+    {/await}
+
+</Container>
