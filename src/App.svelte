955980cb9aa46f7c8cee5eac84f41ae2f64ee05b
Jakob Ecker <jakob.ecker@gmx.at>
2023-02-28 01:46:24 +0100

    fix hide-title for smaller sizes

--- src/App.svelte
+++ src/App.svelte
@@ -68,7 +68,7 @@
     <div slot="left" class="is-vertical-align" class:bd-light={hoversTitle} on:click={home}
          on:mouseenter={()=> hoversTitle=true} on:mouseleave={()=> hoversTitle=false}>
         <Icon src={mdiSilverware} alt="Return of the Menu" size="6"/>
-        <h1 class="hide-sm">Return of the Menu</h1>
+        <h1 class="hide-sm hide-xs">Return of the Menu</h1>
     </div>
     <div slot="center" class="grouped">
         <Button primary on:click={prev}>
