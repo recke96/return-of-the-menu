9bbd6116280dc559a657b4e2dc15778beeffe9bf
recke96 <jakob.ecker@gmx.at>
2023-09-11 15:57:37 +0000

    remove svelte spa

--- src/lib/favorites.ts
+++
@@ -1,6 +0,0 @@
-import {createPersistentStore} from "./persistentStore";
-
-const _favorites = createPersistentStore<string[]>("favorites", []);
-_favorites.update(value => value ? value : []);
-
-export const favorites = _favorites;
