4ead17307006d3d89de8d284c6e3c140c1c006bb
recke96 <jakob.ecker@gmx.at>
2023-09-11 20:10:47 +0000

    try sai cookart and options view

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,12 +1,28 @@
 "use strict";
 
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+const sanitize = require("sanitize-html");
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
+
+    eleventyConfig.addFilter("money", function (value) {
+        const currency = value?.currency ?? "EUR";
+        const currencyStyle = Intl.NumberFormat("de-AT", { style: "currency", currency: currency });
+
+        return currencyStyle.format(typeof value === "number" ? value : value?.amount)
+    });
+
+    eleventyConfig.addFilter("sanitize", function(value){
+        return sanitize(value);
+    });
 
     return {
         dir: {
             input: "src",
-            output: "dist"
+            output: "dist",
         }
     };
 };
