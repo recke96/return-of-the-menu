57824b0c80070bb5b162050d7498b1785e976cfb
Jakob Ecker <jakob.ecker42@gmail.com>
2024-04-04 17:13:41 +0200

    Add tailwindcss typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss';
+import Typography from '@tailwindcss/typography';
 
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-    plugins: [],
+    plugins: [Typography],
 } satisfies Config;
