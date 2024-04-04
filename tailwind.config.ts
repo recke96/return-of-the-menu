b7c8c321e911fe2f805d377904278e3be1e629d8
Jakob Ecker <jakob.ecker42@gmail.com>
2024-04-04 23:48:17 +0200

    Begin themeing implementation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,6 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     plugins: [Typography, DaisyUi],
     daisyui: {
-        themes: ["light", "dark", "darcula"]
+        themes: ["light", "dark", "dracula"]
     }
 } satisfies Config;
