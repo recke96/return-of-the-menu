8e40eeb0d4596fb6906d0b27776b790a95daf461
Jakob Ecker <jakob.ecker42@gmail.com>
2024-04-05 14:53:43 +0200

    Add client-only component to avoid hydration mismatch

---
+++ src/components/ClientOnly.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const isClient = ref(false);
+onMounted(() => (isClient.value = true));
+</script>
+
+<template>
+  <slot v-if="isClient"></slot>
+</template>
--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
 import ThemeDemo from "@components/ThemeDemo.vue";
+import ClientOnly from "@components/ClientOnly.vue";
 import { useStore } from "@nanostores/vue";
 import { $theme } from "@stores/theme";
-import { onMounted, ref } from "vue";
 
 const activeTheme = useStore($theme);
-
-// Avoid hydration mismatch
-const isClient = ref(false);
-onMounted(() => (isClient.value = true));
 </script>
 
 <template>
@@ -17,32 +13,34 @@ onMounted(() => (isClient.value = true));
     <div
       class="dropdown-content rounded-box bg-base-200 text-base-content outline outline-1 outline-black/5 overflow-y-auto border shadow-2x1 w-56"
     >
-      <div v-if="isClient" class="grid grid-cols-1 gap-3 p-3">
-        <button
-          class="outline-base-content text-start outline-offset-4"
-          @click="$theme.set('system')"
-        >
-          <ThemeDemo theme="system" :active-theme />
-        </button>
-        <button
-          class="outline-base-content outline-offset-4 text-start"
-          @click="$theme.set('dark')"
-        >
-          <ThemeDemo theme="dark" :active-theme />
-        </button>
-        <button
-          class="outline-base-content outline-offset-4 text-start"
-          @click="$theme.set('light')"
-        >
-          <ThemeDemo theme="light" :active-theme />
-        </button>
-        <button
-          class="outline-base-content outline-offset-4 text-start"
-          @click="$theme.set('dracula')"
-        >
-          <ThemeDemo theme="dracula" :active-theme />
-        </button>
-      </div>
+      <ClientOnly>
+        <div class="grid grid-cols-1 gap-3 p-3">
+          <button
+            class="outline-base-content text-start outline-offset-4"
+            @click="$theme.set('system')"
+          >
+            <ThemeDemo theme="system" :active-theme />
+          </button>
+          <button
+            class="outline-base-content outline-offset-4 text-start"
+            @click="$theme.set('dark')"
+          >
+            <ThemeDemo theme="dark" :active-theme />
+          </button>
+          <button
+            class="outline-base-content outline-offset-4 text-start"
+            @click="$theme.set('light')"
+          >
+            <ThemeDemo theme="light" :active-theme />
+          </button>
+          <button
+            class="outline-base-content outline-offset-4 text-start"
+            @click="$theme.set('dracula')"
+          >
+            <ThemeDemo theme="dracula" :active-theme />
+          </button>
+        </div>
+      </ClientOnly>
     </div>
   </div>
 </template>
