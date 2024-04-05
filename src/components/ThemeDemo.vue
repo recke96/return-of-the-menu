55c83edb5edc5f366c63fb8e721a1e7eca39e97a
Jakob Ecker <jakob.ecker42@gmail.com>
2024-04-05 14:21:41 +0200

    Finish theme picker

---
+++ src/components/ThemeDemo.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import type { Themes } from "@stores/theme";
+
+const props = defineProps<{
+  theme: Themes;
+  activeTheme?: Themes;
+}>();
+</script>
+
+<template>
+  <span
+    class="bg-base-100 rounded-btn text-base-content block w-full cursor-pointer"
+    :data-theme="props.theme !== 'system' ? props.theme : undefined"
+  >
+    <span class="grid grid-cols-5 grid-rows-3"
+      ><span
+        class="col-span-5 row-span-3 row-start-1 flex items-center gap-2 px-4 py-3"
+        ><svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          :class="[
+            { invisible: theme !== activeTheme },
+            'h-3',
+            'w-3',
+            'shrink-0',
+          ]"
+        >
+          <path
+            d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"
+          ></path>
+        </svg>
+        <span class="flex-grow text-sm">{{ props.theme }}</span>
+        <span class="flex h-full shrink-0 flex-wrap gap-1">
+          <span class="bg-primary rounded-badge w-2"></span>
+          <span class="bg-secondary rounded-badge w-2"></span>
+          <span class="bg-accent rounded-badge w-2"></span>
+          <span class="bg-neutral rounded-badge w-2"></span>
+        </span>
+      </span>
+    </span>
+  </span>
+</template>
--- src/components/ThemePicker.vue
+++ src/components/ThemePicker.vue
@@ -1,24 +1,47 @@
 <script setup lang="ts">
-import ThemeBtn from "./ThemeBtn.vue";
+import ThemeDemo from "@components/ThemeDemo.vue";
+import { useStore } from "@nanostores/vue";
 import { $theme } from "@stores/theme";
+import { onMounted, ref } from "vue";
+
+const activeTheme = useStore($theme);
+
+// Avoid hydration mismatch
+const isClient = ref(false);
+onMounted(() => (isClient.value = true));
 </script>
 
 <template>
   <div class="dropdown dropdown-end">
     <div tabindex="0" role="button" class="btn btn-ghost">Theme</div>
     <div
-      class="dropdown-content bg-base-200 text-base-content overflow-y-auto border shadow-2x1 w-56"
+      class="dropdown-content rounded-box bg-base-200 text-base-content outline outline-1 outline-black/5 overflow-y-auto border shadow-2x1 w-56"
     >
-      <div class="grid grid-cols-1 gap-3 p-3">
+      <div v-if="isClient" class="grid grid-cols-1 gap-3 p-3">
         <button
           class="outline-base-content text-start outline-offset-4"
           @click="$theme.set('system')"
         >
-          System
+          <ThemeDemo theme="system" :active-theme />
         </button>
-        <ThemeBtn theme="dark" @click="$theme.set('dark')" />
-        <ThemeBtn theme="light" @click="$theme.set('light')" />
-        <ThemeBtn theme="dracula" @click="$theme.set('dracula')" />
+        <button
+          class="outline-base-content outline-offset-4 text-start"
+          @click="$theme.set('dark')"
+        >
+          <ThemeDemo theme="dark" :active-theme />
+        </button>
+        <button
+          class="outline-base-content outline-offset-4 text-start"
+          @click="$theme.set('light')"
+        >
+          <ThemeDemo theme="light" :active-theme />
+        </button>
+        <button
+          class="outline-base-content outline-offset-4 text-start"
+          @click="$theme.set('dracula')"
+        >
+          <ThemeDemo theme="dracula" :active-theme />
+        </button>
       </div>
     </div>
   </div>
