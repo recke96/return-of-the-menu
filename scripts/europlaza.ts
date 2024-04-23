76dd9d65bf6f43e5d53019e1939c1c445147aa03
Jakob Ecker <jakob.ecker@gmx.at>
2024-04-23 22:43:08 +0200

    Fix warnings

--- scripts/europlaza.ts
+++ scripts/europlaza.ts
@@ -7,12 +7,6 @@ if (user && password) {
   console.warn("No europlaza user or password set");
 }
 
-const url = {
-  api: "https://europlaza.pockethouse.io/api/graphql",
-  token:
-    "https://europlaza.pockethouse.io/oauth/token?grant_type=client_credentials&scope=read&redirect_uri=https://app.pockethouse.at&response-type=token",
-};
-
 async function load(): Promise<void> {
   console.log("running");
 }
