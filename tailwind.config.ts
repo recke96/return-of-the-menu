68615d06f572ef6354aac6488aeaca28e631d5f4
Jakob Ecker <jakob.ecker@gmx.at>
2024-04-23 20:57:16 +0200

    Layout & typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from "tailwindcss";
+import Typography from "@tailwindcss/typography";
 import DaisyUi from "daisyui";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [DaisyUi],
+  plugins: [Typography, DaisyUi],
   daisyui: {
     themes: ["light", "dark", "dracula"],
   },
